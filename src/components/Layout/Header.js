b0f57c0daf476bf95308853de8514b0fa37b30da
Prajakta Bavikar <prajakta.bavikar@gmail.com>
2022-02-24 23:39:01 -0500

    adding changes

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1> Meal</h1>
+        <h1> Meal App</h1>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
